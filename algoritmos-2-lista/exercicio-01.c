8c4105a302a9528debee62bd3e7af1d99afbc6b8
Matheus Mendes Resende Tomé <theuzae588@gmail.com>
2026-03-30 16:10:25 -0300

    Add files via upload

---
+++ algoritmos-2-lista/exercicio-01.c
@@ -0,0 +1,14 @@
+/* Aluno: Matheus Mendes
+Exercício 1 - Procedimento que exiba "Iniciando o sistema".
+*/
+
+#include <stdio.h>
+
+int main() {
+    mensagem();
+    return 0;
+}
+
+void mensagem() {
+    printf("Iniciando o sistema");
+}
